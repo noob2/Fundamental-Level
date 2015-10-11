c9bbf7c8e113d7be072f0b655df12560db5a4eba
noob2 <viktor.jordanov.1992@gmail.com>
2015-10-11 12:53:11 +0300

    да

---
+++ EncapsulationAndPolymorphysm/AnimalFarm/AnimalFarm.cs
@@ -0,0 +1,14 @@
+﻿namespace AnimalFarm
+{
+    using System;
+
+    public class AnimalFarm
+    {
+        public static void Main()
+        {
+            Chicken chicken = new Chicken("Mara", 3);
+            Console.WriteLine(chicken.ProductPerDay);
+            chicken.GetHumanAge();
+        }
+    }
+}
