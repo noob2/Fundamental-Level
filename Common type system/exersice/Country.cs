f8027894d4909fda07e870e89e15c68beb27da92
noob2 <viktor.jordanov.1992@gmail.com>
2015-11-05 12:11:30 +0200

    common type exercise finished

---
+++ Common type system/exersice/Country.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exersice
+
+{
+    class Country : IComparable<Country>, ICloneable
+    {
+        private string name;
+        private ulong population;
+        private int area;
+
+        public Country(string name, ulong population, int area, params string[] cities)
+        {
+            this.name = Name;
+            this.population = Population;
+            this.area = Area;
+            this.Cities = new HashSet<string>(cities);
+        }
+
+        public string Name { get; set; }
+        public ulong Population { get; set; }
+        public int Area { get; set; }
+        public HashSet<string> Cities { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Country;
+
+            if (object.Equals(other, null))
+            {
+                return other.Name == this.Name;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public int CompareTo(Country other)
+        {
+            if (this.Area == other.Area)
+            {
+                if (this.Population == other.Population)
+                {
+                    return string.Compare(this.Name, other.Name, StringComparison.InvariantCulture);
+                }
+                return this.Population.CompareTo(other.Population);
+            }
+            return this.Area.CompareTo(other.Area);
+        }
+
+        public object Clone()
+        {
+            return new Country(this.Name, this.Population,this.Area, this.Cities.ToArray());
+        }
+
+        public static bool operator ==(Country country, Country country2)
+        {
+            if (country == null)
+            {
+                return false;
+            }
+            return country.Equals(country2);
+        }
+
+        public static bool operator !=(Country country, Country country2)
+        {
+            return !(country == country2);
+        }
+    }
+}
---
+++ Common type system/exersice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country bg = new Country("Bulgaria", 7100000, 111000, "Sofia", "Plovdiv", "Varna");
+            Country usa = new Country("USA", 300000000, 1200000, "New York", "Los Angeles", "San Francisco");
+            Country bg2 = new Country("Bulgaria", 8000000, 10);
+            Country bg3 = new Country("Bulgaria", 8000000, 111000);
+            Country hr = new Country("Croatia", 8000000, 111000);
+
+            var bgCopy = bg.Clone() as Country;
+            bg.Cities.Add("Kaspichan");
+            Console.WriteLine(string.Join(", ", bg.Cities));
+            Console.WriteLine(string.Join(", ", bgCopy.Cities));
+
+        }
+    }
+}
